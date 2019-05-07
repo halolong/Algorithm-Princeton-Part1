29bcb1a0a44a5af058e697c4586fdaa04e14b055
halolong <halolong@hotmail.com>
2019-05-07 17:09:10 +0800

    2019.05.07 Implement Point.java

---
+++ src/week3/assignment/BruteCollinearPoints.java
@@ -0,0 +1,19 @@
+package week3.assignment;
+
+public class BruteCollinearPoints {
+
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+        
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+
+    }
+}
