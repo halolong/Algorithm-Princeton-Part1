aabe29f9bdca685f372dbab2a77b93450a4eb2bf
halolong <halolong@hotmail.com>
2019-05-08 14:51:49 +0800

    2019.05.08 Implement BruteCollinearPoints.java

--- src/week3/assignment/BruteCollinearPoints.java
+++ src/week3/assignment/BruteCollinearPoints.java
@@ -1,19 +1,58 @@
 package week3.assignment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BruteCollinearPoints {
+
+    private LineSegment[] segments;
 
     // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points) {
-        
+        checkDuplicatedEntries(points);
+
+        ArrayList<LineSegment> foundSegments = new ArrayList<>();
+
+        Point[] pointsCopy = Arrays.copyOf(points, points.length);
+
+        Arrays.sort(pointsCopy);
+
+        //找共线的部分
+        for (int p = 0; p < pointsCopy.length; p++) {
+            for (int q = p + 1; q < pointsCopy.length; q++) {
+                for (int r = q + 1; r < pointsCopy.length; r++) {
+                    for (int s = r + 1; s < pointsCopy.length; s++) {
+                        if (pointsCopy[p].slopeTo(pointsCopy[q]) == pointsCopy[p].slopeTo(pointsCopy[r]) &&
+                                pointsCopy[p].slopeTo(pointsCopy[q]) == pointsCopy[p].slopeTo(pointsCopy[s])) {
+                            // 将两个端点加入lineSegment
+                            foundSegments.add(new LineSegment(pointsCopy[p], pointsCopy[s]));
+                        }
+                    }
+                }
+            }
+        }
+        // 通过toArray转换成lineSegment对象
+        segments = foundSegments.toArray(new LineSegment[foundSegments.size()]);
     }
 
     // the number of line segments
     public int numberOfSegments() {
-
+        return segments.length;
     }
 
     // the line segments
     public LineSegment[] segments() {
+        // 相当于返回 segments中存在的line segments
+        return Arrays.copyOf(segments, numberOfSegments());
+    }
 
+    private void checkDuplicatedEntries(Point[] points) {
+        for (int i = 0; i < points.length; i ++) {
+            for (int j = i + 1; j < points.length; j++) {
+                if (points[i].compareTo(points[j]) == 0) {
+                    throw new IllegalArgumentException("Duplicated entries in given points");
+                }
+            }
+        }
     }
 }
